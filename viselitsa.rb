753d2842e5e7e28685782d028624ce2054f68fe6
KuSeMi <SKushnir@i.ua>
2018-12-25 11:10:20 +0200

    releasev1

---
+++ viselitsa.rb
@@ -0,0 +1,41 @@
+# Этот код необходим только при использовании русских букв на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+# Подключаем библиотеку unicode_utils
+# gem install unicode_utils
+require_relative "unicode_utils"
+
+require_relative "game"
+require_relative "result_printer"
+require_relative "word_reader"
+
+current_path = File.dirname(__FILE__ )
+
+puts "Игра виселица. Версия 4\n\n"
+sleep 1
+
+printer = ResultPrinter.new
+
+word_reader = WordReader.new
+
+words_file_name = current_path + "/data/words.txt"
+
+# Все изменения логики будут у нас в классе Game. Нам нужно в двух местах
+# преобразовать полученные данные в верхний регистр, используя метод upcase
+# подключенного модуля UnicodeUtils: при генерации массива букв из полученного
+# слова в конструкторе и при получении новой буквы от пользователя.
+game = Game.new(word_reader.read_from_file(words_file_name))
+
+while game.status == 0
+  printer.print_status(game)
+  game.ask_next_letter
+end
+
+printer.print_status(game)
---
+++ word_reader.rb
@@ -0,0 +1,22 @@
+# Класс WordReader, отвечающий за чтение слова для игры.
+class WordReader
+  def read_from_args
+    return ARGV[0]
+  end
+
+  def read_from_file(file_name)
+    # Проверку наличия файла можно заменить на блок begin-rescue, при этом
+    # ловить только конкретную ошибку обращения к файловой системе.
+    begin
+      file = File.new(file_name, "r:UTF-8")
+      lines = file.readlines
+      file.close
+    rescue SystemCallError
+      # Если файл со словами не удалось открыть, нет смысла играть, поэтому тут
+      # в пору закрыть программу и написать об этом пользователю
+      abort "Файл со словами не найден!"
+     end
+
+    return lines.sample.chomp
+  end
+end
